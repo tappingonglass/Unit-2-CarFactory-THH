41a5722dd1f9b292aea001d700efd48f4b68157d
Todd Hoffman <thoffman@new-host-4.home>
2014-08-31 20:53:09 -0400

    Initial Commit

---
+++ Unit 2 X Code test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit 2 X Code test
+//
+//  Created by Todd Hoffman on 8/31/14.
+//  Copyright (c) 2014 Kokley. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
